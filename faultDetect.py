32e34bef6d8c8a6777e2fd513cc8ef0e9e7b48b0
gsakis <gkyrtsakas@gmail.com>
2014-07-20 15:50:18 -0400

    Script will run every night on server
    -Successfully retrieves local sunrise and sunset times
    -Can send email for daily reports

---
+++ faultDetect.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+
+
+import smtplib
+import urllib2
+import datetime
+import xml.etree.cElementTree as ET
+
+
+#db = MySQLdb.connect("localhost", "root", "root", "data1")
+#cursor = db.cursor()
+#cursor.execute("SELECT * from data1 WHERE DAY(date) = (DAY(NOW())-2) and MONTH(date) = MONTH(NOW())")
+
+message = 	"""From: From Person <from@fromdomain.com>
+			To: To Person <to@todomain.com>
+			MIME-Version: 1.0
+			Content-type: text/html
+			Subject: SMTP HTML e-mail test
+
+			This is an e-mail message to be sent in HTML format
+
+			<b>This is HTML message.</b>
+			<h1>This is headline.</h1>"""
+
+
+fromaddr = 'gkyrtsakas@gmail.com'
+toaddrs  = ['gkyrtsakas@gmail.com']
+msg = "\r\n".join([
+  "From: gkyrtsakas@gmail.com",
+  "To: gkyrtsakas@gmail.com",
+  "Subject: Just a message",
+  "",
+  "Why, oh why"
+  ])
+username = 'gkyrtsakas@gmail.com'
+password = 'uriuxezdztdqxozp'
+try:
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.ehlo()
+	server.starttls()
+	server.login(username,password)
+	server.sendmail(fromaddr, toaddrs, msg)
+	server.close()
+	print "mail sent"
+except:
+	print "failed to send mail"
+
+#http://www.earthtools.org/sun/<latitude>/<longitude>/<day>/<month>/<timezone>/<dst>
+
+def getSRSS():
+	dt = datetime.datetime.now()
+	timezone = "-5/"
+	latitude = "42.088032/"
+	longitude = "-82.705078/"
+	day = ("%s/" %dt.day)
+	month = ("%s/" %dt.month)
+
+	url = "http://www.earthtools.org/sun/" + latitude + longitude + day + month + timezone + "0"
+
+	result = urllib2.urlopen(url).read()
+
+	root = ET.fromstring(result)
+
+	sunrise = root[3][0].text
+
+	sunset = root[4][0].text
+
+	return [sunrise, sunset]
